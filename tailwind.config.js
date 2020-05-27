020206bdf90222d8982c0ef0fa9308169079d213
bdombar <bdombar@gmail.com>
2020-05-27 17:53:03 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./src/**/*.html"],
+  theme: {
+    fontFamily: {
+      'body': ['Roboto'],
+      'classic': ['Playfair Display'],
+      'quote': ['Special Elite']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
